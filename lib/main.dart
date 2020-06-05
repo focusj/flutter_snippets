47b7530371fe598a315543c521d43e12e4f243f0
wangkang.kangwang <wangkang.kangwang@bytedance.com>
2020-06-05 17:02:04 +0800

    extract to standalone file
    Change-Id: I4b9ff2a65364483698efb589f22e3c4645fab439

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_snippets/picture_app.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,77 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Layout Demo',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Return Date Example'),
-      ),
-      body: Center(
-        child: SelectionButton(),
-      ),
-    );
-  }
-}
-
-class SelectionButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: Text("Pick An Option"),
-    );
-  }
-
-  _navigateAndDisplaySelection(BuildContext context) async {
-    final result = await Navigator.push(
-        context, MaterialPageRoute(builder: (context) => SelectionScreen()));
-    Scaffold.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text("$result")));
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Select An Option"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: Text('Yep!'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context, "Nope");
-                },
-                child: Text('Nope!'),
-              ),
-            )
-          ],
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
